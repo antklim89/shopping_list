9cfee2e8e1198d60b3403467bfb74500da8305ca
anton <antklim89@mail.ru>
2021-06-20 14:23:35 +0300

    add animation

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite';
-import { FC } from 'react';
+import { useRef, FC } from 'react';
+
 
 import { ReactComponent as CartIcon } from '../assets/cart.svg';
 import { ReactComponent as DoneIcon } from '../assets/done.svg';
@@ -12,64 +13,72 @@ import { useStore } from './StoreProvider';
 
 export const ProductItem: FC<{ product: ProductItemStore }> = observer(({ product }) => {
     const store = useStore();
+    const ref = useRef<HTMLLIElement>(null);
 
     return (
-        <div>
-            <li className={style.item}>
-                <div className={style.isBought}>
-                    {product.isBought ? (
-                        <button type="button" onClick={() => product.update({ isBought: false })}>
-                            <DoneIcon />
-                        </button>
-                    ) : (
-                        <button type="button" onClick={() => product.update({ isBought: true })}>
-                            <CartIcon />
-                        </button>
-                    )}
-                </div>
-                <input
-                    className={style.name}
-                    disabled={product.isBought}
-                    placeholder="Enter product name..."
-                    type="text"
-                    value={product.name}
-                    onChange={(e) => product.update({ name: e.target.value })}
-                    onKeyPress={(e) => { if (e.key === 'Enter') e.currentTarget.blur(); }}
-                />
-                <input
-                    className={style.qte}
-                    disabled={product.isBought}
-                    min={1}
-                    type="number"
-                    value={product.qty}
-                    onChange={(e) => {
-                        const value = parseInt(e.target.value, 10);
-                        if (Number.isNaN(value)) product.update({ qty: 0 });
-                        else product.update({ qty: value });
-                    }}
-                />
-                <select
-                    className={style.unit}
-                    defaultValue={product.unit}
-                    disabled={product.isBought}
-                    onChange={(e) => product.update({ unit: e.target.value as Unit })}
-                >
-                    <option value="kg">kg</option>
-                    <option value="mg">mg</option>
-                    <option value="gram">gram</option>
-                    <option value="liter">liter</option>
-                    <option value="ml">ml</option>
-                    <option value="piece">piece</option>
-                </select>
-                <button
-                    className={style.deleteBtn}
-                    type="button"
-                    onClick={() => store.removeProduct(product)}
-                >
-                    &times;
+        <li
+            className={`${style.item} ${style.appearanceStyle}`}
+            ref={ref}
+        >
+            <input
+                className={style.name}
+                disabled={product.isBought}
+                placeholder="Enter product name..."
+                type="text"
+                value={product.name}
+                onChange={(e) => product.update({ name: e.target.value })}
+                onKeyPress={(e) => { if (e.key === 'Enter') e.currentTarget.blur(); }}
+            />
+            <div className={style.isBought}>
+                {product.isBought ? (
+                    <button type="button" onClick={() => product.update({ isBought: false })}>
+                        <DoneIcon />
+                    </button>
+                ) : (
+                    <button type="button" onClick={() => product.update({ isBought: true })}>
+                        <CartIcon />
+                    </button>
+                )}
+            </div>
+            <input
+                className={style.qte}
+                disabled={product.isBought}
+                min={1}
+                type="number"
+                value={product.qty}
+                onChange={(e) => {
+                    const value = parseInt(e.target.value, 10);
+                    if (Number.isNaN(value)) product.update({ qty: 0 });
+                    else product.update({ qty: value });
+                }}
+            />
+            <select
+                className={style.unit}
+                defaultValue={product.unit}
+                disabled={product.isBought}
+                onChange={(e) => product.update({ unit: e.target.value as Unit })}
+            >
+                <option value="kg">kg</option>
+                <option value="mg">mg</option>
+                <option value="gram">gram</option>
+                <option value="liter">liter</option>
+                <option value="ml">ml</option>
+                <option value="piece">piece</option>
+            </select>
+            <button
+                className={style.deleteBtn}
+                type="button"
+                onClick={() => {
+                    ref.current?.classList.remove(style.appearanceStyle);
+                    ref.current?.classList.add(style.removeStyle);
+                    setTimeout(() => {
+                        store.removeProduct(product);
+                    }, 190);
+                }}
+            >
+                &times;
 
-                </button>
-            </li>
-        </div>
+            </button>
+        </li>
     );
 });
