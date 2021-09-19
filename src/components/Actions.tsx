7942a807f8a5c8f63a15b3dea0211329dfed9d93
Anton <antklim89@mail.ru>
2021-09-19 13:58:52 +0300

    update Actions component

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -26,55 +26,61 @@ const Actions: FunctionComponent = () => {
 
     return (
         <div className="Actions">
-            <button
-                className="Actions__action btn"
-                type="button"
-                onClick={() => share()}
-            >
-                Share
-            </button>
-            <button
-                className="Actions__action btn"
-                type="button"
-                onClick={() => copyTextToClipboard(location.href)}
-            >
-                Copy link
-            </button>
-            <button
-                className="Actions__action btn"
-                type="button"
-                onClick={() => clearList()}
-            >
-                Clear List
-            </button>
-            <button
-                className="Actions__action btn"
-                type="button"
-                onClick={() => toggleBougth()}
-            >
-                Toggle all bought
-            </button>
-            <button
-                className="Actions__action btn"
-                type="button"
-                onClick={() => toFile()}
-            >
-                Save to File
-            </button>
+            <div className="Actions__group">
+                <button
+                    className="Actions__action btn"
+                    type="button"
+                    onClick={() => toggleBougth()}
+                >
+                    Toggle all bought
+                </button>
+                <button
+                    className="Actions__action btn"
+                    type="button"
+                    onClick={() => copyTextToClipboard(location.href)}
+                >
+                    Copy link
+                </button>
+                <button
+                    className="Actions__action btn"
+                    type="button"
+                    onClick={() => clearList()}
+                >
+                    Clear List
+                </button>
+            </div>
+            <div className="Actions__group">
+                {typeof window !== 'undefined' && window?.navigator?.share && (
+                    <button
+                        className="Actions__action btn"
+                        type="button"
+                        onClick={() => share()}
+                    >
+                        Share
+                    </button>
+                )}
+                <button
+                    className="Actions__action btn"
+                    type="button"
+                    onClick={() => toFile()}
+                >
+                    Save to File
+                </button>
 
-            <label className="Actions__action btn" htmlFor="load-file">
-                Load from file
-            </label>
-            <input
-                accept="application/json"
-                className="hidden"
-                id="load-file"
-                type="file"
-                onInput={(e) => {
-                    fromFile(e.currentTarget.files);
-                    e.currentTarget.value = '';
-                }}
-            />
+                <label className="Actions__action btn" htmlFor="load-file">
+                    Load from file
+                </label>
+                <input
+                    accept="application/json"
+                    className="hidden"
+                    id="load-file"
+                    type="file"
+                    onInput={(e) => {
+                        fromFile(e.currentTarget.files);
+                        e.currentTarget.value = '';
+                    }}
+                />
+            </div>
         </div>
     );
 };
