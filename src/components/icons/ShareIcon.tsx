7d2473227ab8d18f7aaf291784a299aa2b0a6b04
Anton <31469798+antklim89@users.noreply.github.com>
2021-10-10 16:16:26 +0000

    fix ShareIcon

--- src/components/icons/ShareIcon.tsx
+++ src/components/icons/ShareIcon.tsx
@@ -4,9 +4,9 @@ import { FunctionComponent, h } from 'preact';
 export const ShareIcon: FunctionComponent = (props) => {
     return (
         <svg
-            height="24"
-            viewBox="0 0 24 24"
-            width="24"
+            height="32"
+            viewBox="0 0 32 32"
+            width="32"
             xmlns="http://www.w3.org/2000/svg"
             {...props}
         >
