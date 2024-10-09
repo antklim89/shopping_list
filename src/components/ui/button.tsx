7d1b1f905427a071e61703c5f2c3cfea9d69fc81
Anton <=>
2024-10-09 15:37:16 +0300

    fix: add forwardRef

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,13 +1,15 @@
-import type { ComponentProps } from 'react';
+import { type ComponentProps, forwardRef } from 'react';
 import { cn } from '@/lib/utils';
 
 
-export function Button({ className, ...props }: ComponentProps<'button'>) {
+export const Button = forwardRef<HTMLButtonElement, ComponentProps<'button'>>(({ className, ...props }, ref) => {
   return (
     <button
+      ref={ref}
       type="button"
       {...props}
       className={cn('bg-primary-500 hover:bg-primary-600 text-white border-2 border-primary-200 px-6 py-2 transition-colors uppercase', className)}
     />
   );
-}
+});
+Button.displayName = 'Button';
