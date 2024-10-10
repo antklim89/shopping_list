5df35c67a65f6d0a54124723b6f6be9e716dbdb0
Anton <=>
2024-10-10 19:58:27 +0300

    feat: create generateId function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function generateId(): string {
+  return (Math.random() * 1000).toString(36);
+}
