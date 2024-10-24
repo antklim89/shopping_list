4e3490725d3ae38e91a7794dd6e20b0995297f51
Anton <=>
2024-10-24 16:01:53 +0300

    fix(styles): update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,23 +1,19 @@
 import { CollectionDrawer } from '@/components/collection-drawer';
-import { Button } from '@/components/ui/button';
 
 
 export function Header() {
   return (
-    <header className="container flex items-center justify-between p-4">
-      <h1 className="text-2xl font-bold">Shopping List</h1>
-      <nav>
-        <ul className="flex space-x-4">
-          <li>
-            <Button>
-              Create New List
-            </Button>
-          </li>
-          <li>
-            <CollectionDrawer />
-          </li>
-        </ul>
-      </nav>
+    <header className="bg-white p-4 mb-8 shadow-lg">
+      <div className="container flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Shopping List</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <CollectionDrawer />
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
