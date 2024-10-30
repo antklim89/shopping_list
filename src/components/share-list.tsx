304b5ff86c6f9f5ca3f954bf3322da2ea70f56da
Anton <=>
2024-10-30 18:36:40 +0300

    feat: add share

--- src/components/share-list.tsx
+++ src/components/share-list.tsx
@@ -1,3 +1,4 @@
+import { FaCopy, FaShare } from 'react-icons/fa6';
 import { useStore } from '@/lib/store';
 import type { ListShareType } from '@/lib/types';
 import { Button } from './ui/button';
@@ -8,19 +9,35 @@ export function ShareList() {
   const list = useStore(state => state.lists[currentListId]);
 
   if (list == null) return null;
-  if (navigator.share == null) return null;
+  if (navigator.share == null && navigator.share == null) return null;
 
   const share = async () => {
     const shareData: ListShareType = { id: currentListId, list };
     const url = `${location.href}?share=${encodeURIComponent(JSON.stringify(shareData))}`;
+
     try {
-      await navigator.share({ url });
+      if (navigator.share != null) await navigator.share({ url });
+      else if (navigator.share != null) await navigator.clipboard.writeText(url);
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <Button onClick={async () => share()}>Share</Button>
+    <Button onClick={async () => share()}>
+      {navigator.share == null
+        ? (
+            <>
+              <FaCopy />
+              <span className="ml-2 hidden md:inline">Copy URL</span>
+            </>
+          )
+        : (
+            <>
+              <FaShare />
+              <span className="ml-2 hidden md:inline">Share</span>
+            </>
+          )}
+    </Button>
   );
 }
