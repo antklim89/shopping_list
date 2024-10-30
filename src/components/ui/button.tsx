5df3da54208fbf98ffbb3afcb76a676506e64af0
Anton <=>
2024-10-30 21:37:28 +0300

    feat(styles): remove shadow and border

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,7 +8,7 @@ export const Button = forwardRef<HTMLButtonElement, ComponentProps<'button'>>(({
       ref={ref}
       type="button"
       {...props}
-      className={cn('flex justify-center items-center disabled:bg-slate-300 bg-primary-500 hover:bg-primary-600 text-white border-2 border-primary-200 px-6 py-2 transition-colors uppercase shadow-lg', className)}
+      className={cn('flex justify-center items-center disabled:bg-slate-300 bg-primary-500 hover:bg-primary-600 text-white px-6 py-2 transition-colors uppercase', className)}
     />
   );
 });
