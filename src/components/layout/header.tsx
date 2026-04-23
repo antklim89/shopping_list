3284431a45496e3299c07918bbf226721cad439f
Anton <antklim89@mail.ru>
2026-04-23 10:15:45 +0000

    refactor(styles): make header smaller

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,9 +6,9 @@ export function Header() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <header className="mb-8 bg-primary p-4 shadow-lg">
+    <header className="mb-4 bg-primary p-2 shadow-lg">
       <div className="container flex items-center justify-between">
-        <h1 className="font-bold text-2xl text-primary-foreground uppercase">Shopping List</h1>
+        <h1 className="font-bold text-2xl text-primary-foreground">Shopping List</h1>
         <div className="flex space-x-4">
           <button aria-label="Toggle dark mode" className="btn-primary" type="button" onClick={() => toggleTheme()}>
             {theme === 'dark' ? <FaSun /> : <FaMoon />}
